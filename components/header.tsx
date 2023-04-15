fab110547ccb84f9a9f0f8ad0143249b58dec669
Jonathan Ruffles <jruffles1@sheffield.ac.uk>
2023-04-15 20:42:55 +0100

    Updated navbar to only have relevant links

--- components/header.tsx
+++ components/header.tsx
@@ -10,95 +10,86 @@ import headerNavLinks from '../data/navLinks'
 // component that works on pages which support both client and server side
 // rendering, and avoids any flash incorrect content on initial page load.
 export default function Header() {
-  const {data: session, status} = useSession()
-  const loading = status === "loading"
+    const {data: session, status} = useSession()
+    const loading = status === "loading"
 
-  return (
-      <SectionContainer>
-        <header className="flex items-center justify-between py-10">
-          <div>
-            <Link href="/" aria-label={siteMetadata.headerTitle}>
-              <div className="flex items-center justify-between">
-                {typeof siteMetadata.headerTitle === 'string' ? (
-                    <div className="hidden h-6 text-2xl font-semibold sm:block">
-                      {siteMetadata.headerTitle}
+    return (
+        <SectionContainer>
+            <header className="flex items-center justify-between py-10">
+                <Link href="/" aria-label={siteMetadata.headerTitle}
+                      className="flex title-font font-medium items-center mb-4 md:mb-0 ml-3 text-xl">
+                    {siteMetadata.headerTitle}
+                </Link>
+
+                <div className="flex items-center text-base leading-5">
+                    <div className="hidden sm:block">
+                        {headerNavLinks.map((link) => (
+                            <Link
+                                key={link.title}
+                                href={link.href}
+                                className="p-1 font-medium sm:p-4"
+                                hidden={link.authorize && !session?.user}
+                            >
+                                {link.title}
+                            </Link>
+                        ))}
                     </div>
-                ) : (
-                    siteMetadata.headerTitle
-                )}
-              </div>
-            </Link>
-          </div>
-          <div className="flex items-center text-base leading-5">
-            <div className="hidden sm:block">
-              {headerNavLinks.map((link) => (
-                  <Link
-                      key={link.title}
-                      href={link.href}
-                      className="p-1 font-medium sm:p-4"
-                  >
-                    {link.title}
-                  </Link>
-              ))}
-            </div>
-            <ThemeSwitch/>
-          </div>
-        </header>
 
-        <noscript>
-          <style>{`.nojs-show { opacity: 1; top: 0; }`}</style>
-        </noscript>
+                    <ThemeSwitch/>
+                </div>
+            </header>
 
-        <div className={styles.signedInStatus + " pb-5"}>
-          <p
-              className={`nojs-show ${
-                  !session && loading ? styles.loading : styles.loaded
-              }`}
-          >
-            {!session && (
-                <>
+            <noscript>
+                <style>{`.nojs-show { opacity: 1; top: 0; }`}</style>
+            </noscript>
+
+            <div className={styles.signedInStatus + " pb-5"}>
+                <p
+                    className={`nojs-show ${!session && loading ? styles.loading : styles.loaded}`}>
+                    {!session && (
+                        <>
               <span className={styles.notSignedInText}>
                 You are not signed in
               </span>
-                  <a
-                      href={`/api/auth/signin`}
-                      className={styles.buttonPrimary}
-                      onClick={(e) => {
-                        e.preventDefault()
-                        signIn()
-                      }}
-                  >
-                    Sign in
-                  </a>
-                </>
-            )}
-            {session?.user && (
-                <>
-                  {session.user.image && (
-                      <span
-                          style={{backgroundImage: `url('${session.user.image}')`}}
-                          className={styles.avatar}
-                      />
-                  )}
-                  <span className={styles.signedInText}>
+                            <a
+                                href={`/api/auth/signin`}
+                                className={styles.buttonPrimary}
+                                onClick={(e) => {
+                                    e.preventDefault()
+                                    signIn()
+                                }}
+                            >
+                                Sign in
+                            </a>
+                        </>
+                    )}
+                    {session?.user && (
+                        <>
+                            {session.user.image && (
+                                <span
+                                    style={{backgroundImage: `url('${session.user.image}')`}}
+                                    className={styles.avatar}
+                                />
+                            )}
+                            <span className={styles.signedInText}>
                 <small>Signed in as</small>
                 <br/>
                 <strong>{session.user.email ?? session.user.name}</strong>
               </span>
-                  <a
-                      href={`/api/auth/signout`}
-                      className={styles.button}
-                      onClick={(e) => {
-                        e.preventDefault()
-                        signOut()
-                      }}
-                  >
-                    Sign out
-                  </a>
-                </>
-            )}
-          </p>
-        </div>
-      </SectionContainer>
-  )
+                            <a
+                                href={`/api/auth/signout`}
+                                className={styles.button}
+                                onClick={(e) => {
+                                    e.preventDefault()
+                                    signOut()
+                                }}
+                            >
+                                Sign out
+                            </a>
+                        </>
+                    )}
+                </p>
+            </div>
+        </SectionContainer>
+    )
 }
--- data/navLinks.js
+++ data/navLinks.js
@@ -1,11 +1,13 @@
 const headerNavLinks = [
-    { href: '/', title: 'Home' },
-    { href: '/client', title: 'Client' },
-    { href: '/server', title: 'Server' },
-    { href: '/protected', title: 'Protected' },
-    { href: '/api-example', title: 'API' },
-    { href: '/admin', title: 'Admin' },
-    { href: '/me', title: 'Me' },
+    { href: '/', title: 'Home', authorize: false },
+    { href: '/discover', title: 'Discover', authorize: false },
+    { href: '/profile', title: 'My Profile', authorize: true },
+
+    // { href: '/server', title: 'Server' },
+    // { href: '/protected', title: 'Protected' },
+    // { href: '/api-example', title: 'API' },
+    // { href: '/admin', title: 'Admin' },
+    // { href: '/me', title: 'Me' },
 ]
 
 export default headerNavLinks
