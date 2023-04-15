de6373fdb41d7bf8673f65a0f5e702b2ab056fcc
Jonathan Ruffles <jruffles1@sheffield.ac.uk>
2023-04-15 20:43:43 +0100

    Added template profile page

---
+++ pages/profile.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Layout from "../components/layout";
+import {useSession} from "next-auth/react";
+import AccessDenied from "../components/access-denied";
+
+export default function Profile() {
+
+    const { data: session } = useSession()
+
+    // If no session exists, display access denied message
+    if (!session) {
+        return (
+            <Layout>
+                <AccessDenied />
+            </Layout>
+        )
+    }
+
+    return (
+        <Layout>
+            <h1 className="text-3xl font-bold text-center">
+                {session?.user.name}
+            </h1>
+        </Layout>
+    )
+
+}
