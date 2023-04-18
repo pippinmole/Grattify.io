0f12914b765e76b16a537eaff98dd448a25b994b
Jonathan Ruffles <jruffles1@sheffield.ac.uk>
2023-04-18 01:47:23 +0100

    Fixed tiny jitter between components on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,11 +11,11 @@ import {authOptions} from "./api/auth/[...nextauth]";
 export default function IndexPage({post}: { post: IPost }) {
     const {data: session, status} = useSession()
 
-    const parsedPost = JSON.parse(post as unknown as string) as IPost
+    post = JSON.parse(post as unknown as string) as IPost
 
     return (
         <Layout>
-            {parsedPost && session?.user ? <ExistingPost post={parsedPost}/> : <CreatePost/>}
+            {post ? <ExistingPost post={post}/> : <CreatePost/>}
 
             <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"/>
 
