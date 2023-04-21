c935ee3dc8ed9960c42701ff1b3c96349885f266
Jonathan Ruffles <jruffles1@sheffield.ac.uk>
2023-04-21 16:40:01 +0100

    Fixed authorized route giving wrong redirect if you did a hard-reload on an authorized route

--- components/authorized.tsx
+++ components/authorized.tsx
@@ -1,20 +1,22 @@
 import {useRouter} from "next/router";
 import {useEffect} from "react";
-import {useSession} from "@supabase/auth-helpers-react";
+import {useSessionContext} from "@supabase/auth-helpers-react";
 
 export default function Authorized({children}: { children: React.ReactNode}) {
 
   const router = useRouter()
-  const session = useSession()
+  const { isLoading, session, error } = useSessionContext();
   const {isReady} = router
 
   useEffect(() => {
-    if(!isReady) return
+    if(!isReady || isLoading) {
+      return
+    }
 
     if(!session) {
       router.replace("/")
     }
-  }, [isReady, session])
+  }, [isReady, session, isLoading])
 
   return (
     <>
